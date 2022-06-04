2217d5c56956f40aa244e91c0bc98e51b9bce11b
Roger <morenoroger001@gmail.com>
2022-06-04 18:44:31 -0400

    feat: Add Token_onBoard class
    this class will represent the tokens played by players

--- DominoLibrary/Tokens.cs
+++ DominoLibrary/Tokens.cs
@@ -12,3 +12,17 @@ class Token
     }
     
 }
+
+class Token_onBoard : Token
+{
+    public bool Straight { get; private set; }
+    public IPlayer Owner { get; private set; }
+    public bool PlayRight { get; private set; }
+
+    public Token_onBoard(int left, int right, bool straight, IPlayer owner, bool playRight) : base(left, right)
+    {
+        Straight = straight;
+        Owner = owner;
+        PlayRight = playRight;
+    }
+}
