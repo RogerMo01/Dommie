7bf3462162c21613774ee5460512992061c88a7b
Roger <morenoroger001@gmail.com>
2022-06-05 21:48:18 -0400

    feat: Set constructor and properties

--- DominoLibrary/Board.cs
+++ DominoLibrary/Board.cs
@@ -3,15 +3,28 @@ namespace DominoLibrary;
 
 public class Board
 {
+    CircularList<IPlayer> Players;
+    List<Token> GameTokens;
+    Dictionary<IPlayer, List<Token>> PlayersTokens;
+    LinkedList<Token> BoardTokens;
+    Setting Settings;
+
     public Board(Setting setting)
     {
-        
+        Settings = setting;
+
+        Players = setting.Players;
+        GameTokens = GenerateTokens(setting.MaxToken);
+        PlayersTokens = HandOut(GameTokens);
+        BoardTokens = new LinkedList<Token>();
     }
 
     public GameResult Start()
     {
-
+        
     }
+
+
 
     private static List<Token> GenerateTokens(int maxToken)
     {
