907b4b47c7343ccee4859094e03c352204a8e94b
Roger <morenoroger001@gmail.com>
2022-06-05 21:51:13 -0400

    feat: Change IPlayer to Node<IPlayer>
    change made in Setting class

--- DominoLibrary/Setting.cs
+++ DominoLibrary/Setting.cs
@@ -7,9 +7,9 @@ public class Setting
     public CircularList<IPlayer> Players { get; private set; }
     public IGameOver OverCondition { get; private set; }
     public int MaxToken { get; private set; }
-    public IPlayer Inner { get; private set; }
+    public Node<IPlayer> Inner { get; private set; }
 
-    public Setting(CircularList<IPlayer> players, IGameOver overCondition, int maxToken, IPlayer inner)
+    public Setting(CircularList<IPlayer> players, IGameOver overCondition, int maxToken, Node<IPlayer> inner)
     {
         Players = players;
         OverCondition = overCondition;
