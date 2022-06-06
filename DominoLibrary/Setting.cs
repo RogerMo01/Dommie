07ab8bae4caea3d2752ecce46f70d6d386535dba
Roger <morenoroger001@gmail.com>
2022-06-05 20:51:11 -0400

    feat: Implement Setting class

--- DominoLibrary/Setting.cs
+++ DominoLibrary/Setting.cs
@@ -1,6 +1,19 @@
+using Utils;
+
 namespace DominoLibrary;
 
 public class Setting
 {
+    public CircularList<IPlayer> Players { get; private set; }
+    public IGameOver OverCondition { get; private set; }
+    public int MaxToken { get; private set; }
+    public IPlayer Inner { get; private set; }
 
+    public Setting(CircularList<IPlayer> players, IGameOver overCondition, int maxToken, IPlayer inner)
+    {
+        Players = players;
+        OverCondition = overCondition;
+        MaxToken = maxToken;
+        Inner = inner;
+    }
 }
