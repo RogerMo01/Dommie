0db4e0b372c8d5787493e83f3b5aa11b1119637e
ClauAlvarez01 <clau200103am@gmail.com>
2022-06-05 21:28:15 -0700

    feat: Implement GenerateTokens method

--- DominoLibrary/Board.cs
+++ DominoLibrary/Board.cs
@@ -15,7 +15,17 @@ public class Board
 
     private static List<Token> GenerateTokens(int maxToken)
     {
+        List<Token> gameTokens = new List<Token>();
 
+        for(int i = -1; i < maxToken; i++)
+        {
+            for (int j = i + 1; j <= maxToken; j++)
+            {
+                gameTokens.Add(new Token(i + 1, j));
+            }
+        }
+
+        return gameTokens;    
     }
 
     private static Dictionary<IPlayer, List<Token>> HandOut(List<Token> tokens)
