2b25b5859bca263c32e274ff54c83d7434b529ac
Roger <morenoroger001@gmail.com>
2022-06-12 19:52:17 -0400

    feat: Remove IGameOver usages
    Don't use IGameOver till now

--- DominoLibrary/Setting.cs
+++ DominoLibrary/Setting.cs
@@ -5,14 +5,12 @@ namespace DominoLibrary;
 public class Setting
 {
     public CircularList<IPlayer> Players { get; private set; }
-    public IGameOver OverCondition { get; private set; }
     public int MaxToken { get; private set; }
     public Node<IPlayer> Inner { get; private set; }
 
-    public Setting(CircularList<IPlayer> players, IGameOver overCondition, int maxToken, Node<IPlayer> inner)
+    public Setting(CircularList<IPlayer> players, int maxToken, Node<IPlayer> inner)
     {
         Players = players;
-        OverCondition = overCondition;
         MaxToken = maxToken;
         Inner = inner;
     }
