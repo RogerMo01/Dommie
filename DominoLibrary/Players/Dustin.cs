c44ea6516f5a14e580c5906e7190e27b537dda99
ClauAlvarez01 <clau200103am@gmail.com>
2022-06-12 21:39:26 -0700

    feat: Implement Dustin player
    The classic "bota-gorda" player

--- DominoLibrary/Players/Dustin.cs
+++ DominoLibrary/Players/Dustin.cs
@@ -15,6 +15,51 @@ public class Dustin : IPlayer
 
     public Token_onBoard Play(PlayInfo info)
     {
-        throw new NotImplementedException();
+        Token result = info.Tokens[0];
+        int[] ends = Board.GetEnds(info.BoardTokens);
+        int points = 0;
+
+        foreach (var token in info.Tokens)
+        {
+            int value = 0;
+            if(Playable(token, ends))
+            {
+                value = token.Left + token.Right;
+                if(value > points)
+                {
+                    points = value;
+                    result = token;  
+                }
+            }
+        }
+
+        bool playRight = PlayRight(result, ends);
+        bool straight = Straight(result, ends);
+
+        return new Token_onBoard(result.Left, result.Right, straight, this, playRight);
     }
+
+    private bool Playable(Token token, int[] ends)
+    {
+        foreach (var item in ends)
+        {
+            if(item == token.Left || item == token.Right) return true;
+        }
+
+        return false;
+    }
+
+    private bool PlayRight(Token token, int[] ends)
+    {
+        if(ends[1] == token.Left || ends[1] == token.Right) return true;
+        
+        return false;  
+    }
+
+    private bool Straight(Token token, int[] ends)
+    {
+        if((ends[1] == token.Left) || (ends[0] == token.Right )) return true;
+
+        return false; 
+    } 
 }
