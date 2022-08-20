4b35ae53cca80f7335bda09f269826cab96ada01
ClauAlvarez01 <clau200103am@gmail.com>
2022-08-20 00:00:58 -0700

    feat: Create new abstraction
    Implement different options to choose who starts a game

---
+++ DominoLibrary/InnerPlayer.cs
@@ -0,0 +1,65 @@
+namespace DominoLibrary;
+
+public delegate IPlayer Inner(Dictionary<IPlayer, List<Token>> PlayersTokens);
+
+public static class InnerPlayer
+{
+    public static IPlayer Random_Inner(Dictionary<IPlayer, List<Token>> PlayersTokens)
+    {
+        IPlayer[] players = PlayersTokens.Keys.ToArray();
+        IPlayer inner = players[0];
+        
+        Random random = new Random();
+
+        return players[random.Next(players.Length - 1)];
+    }
+
+    public static IPlayer Bigger_Token(Dictionary<IPlayer, List<Token>> PlayersTokens)
+    {
+        IPlayer[] players = PlayersTokens.Keys.ToArray();
+        IPlayer inner = players[0];
+
+        Token currentBigger = PlayersTokens[inner].First();
+
+        foreach (var item in players)
+        {
+            List<Token> tokens = PlayersTokens[item];
+            tokens.Sort();
+
+            if(tokens[tokens.Count - 1].Points > currentBigger.Points)
+            {
+                currentBigger = tokens[tokens.Count - 1];
+                inner = item;
+            }
+        }
+
+        return inner;
+    }
+
+    public static IPlayer Min_Double(Dictionary<IPlayer, List<Token>> PlayersTokens)
+    {
+        IPlayer[] players = PlayersTokens.Keys.ToArray();
+        IPlayer inner = players[0];
+        
+        Token token = PlayersTokens[inner].First();
+
+        bool isDouble = false;
+
+        foreach (var player in players)
+        {
+            foreach (var item in PlayersTokens[player])
+            {
+                if((item.Left.Equals(item.Right)) && (item.Points <= token.Points))
+                {
+                    token = item; 
+                    inner = player;
+                    isDouble = true;
+                }
+            }
+        }
+
+        if(!isDouble) return Random_Inner(PlayersTokens);
+
+        return inner;
+    }
+}
