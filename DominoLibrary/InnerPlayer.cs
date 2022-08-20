6c964b6f21ea38b23cefe18bd88ac582bf5074c4
ClauAlvarez01 <clau200103am@gmail.com>
2022-08-20 16:11:46 -0700

    feat: Implement Max_Data method
    New way to choose the inner

--- DominoLibrary/InnerPlayer.cs
+++ DominoLibrary/InnerPlayer.cs
@@ -62,4 +62,65 @@ public static class InnerPlayer
 
         return inner;
     }
+
+    
+    public static IPlayer Max_Data(Dictionary<IPlayer, List<Token>> PlayersTokens)
+    {
+        int maxNumberToken = Max_NumberToken(PlayersTokens);
+        IPlayer[] players = PlayersTokens.Keys.ToArray();
+        List<(IPlayer, int[])> playersCountTokens = PlayersCountTokens(PlayersTokens, players, maxNumberToken);
+        
+        int aux = 0;
+        IPlayer result = players[0];
+
+        for (int i = 0; i < players.Length; i++)
+        {
+            int max = playersCountTokens[i].Item2.Max();
+
+            if(max > aux) 
+            {
+                aux = max;
+                result = players[i];
+            }
+        }
+
+        return result;
+    }
+
+    private static int Max_NumberToken(Dictionary<IPlayer, List<Token>> PlayersTokens)
+    {
+        int result = 0;
+        IPlayer[] players = PlayersTokens.Keys.ToArray();
+
+        foreach (var player in players)
+        {
+            foreach (var token in PlayersTokens[player])
+            {
+                int aux = Math.Max(token.Left, token.Right);
+                if(aux > result) result = aux;
+            }
+        }
+        
+        return result;
+    }
+
+    private static List<(IPlayer, int[])> PlayersCountTokens(Dictionary<IPlayer, List<Token>> PlayersTokens, IPlayer[] players, int maxNumberToken)
+    {
+        List<(IPlayer, int[])> playersCountTokens = new List<(IPlayer, int[])>();
+
+        foreach (var player in players)
+        {
+            int[] numbers = new int[maxNumberToken + 1];
+
+            foreach (var token in PlayersTokens[player])
+            {
+                numbers[token.Left] ++;
+                numbers[token.Right] ++;
+            }
+
+            playersCountTokens.Add((player, numbers));
+        }
+
+        return playersCountTokens;
+    }
 }
